48e8748b4a5855ded32d0a8ab3bfdae93c3f6cb7
Bart <kmiecikjob@gmail.com>
2023-06-25 20:32:23 +0200

    Day 18Exercise 2

--- Day18/main.py
+++ Day18/main.py
@@ -1,15 +1,23 @@
 from turtle import Screen, Turtle
-
+import random
 tim = Turtle()
 tim.shape("turtle")
 tim.color("red")
 
-for _ in range(10):
-    tim.pd()
-    tim.forward(10)
-    tim.pu()
-    tim.forward(10)
-
-
 screen = Screen()
+
+def draw_shape(corners: int):
+    r = random.randint(0, 256)
+    g = random.randint(0, 256)
+    b = random.randint(0, 256)
+    screen.colormode(255)
+    tim.pencolor((r, g, b))
+    tim.pd()
+    for i in range(corners):
+        tim.forward(100)
+        tim.right(360 / corners)
+
+for i in range(3, 11):
+    draw_shape(i)
+
 screen.exitonclick()
