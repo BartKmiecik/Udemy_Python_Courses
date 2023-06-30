3a6a8eb833212ea6de9a3be4693d3a7da72863e1
Bart <kmiecikjob@gmail.com>
2023-06-30 17:35:32 +0200

    Day 23Saving high score locally and reading it

--- Day23/Snake+Project+Code+from+Day+21/scoreboard.py
+++ Day23/Snake+Project+Code+from+Day+21/scoreboard.py
@@ -13,14 +13,23 @@ class Scoreboard(Turtle):
         self.penup()
         self.goto(0, 270)
         self.hideturtle()
+        try:
+            with open('highscore.txt', 'r') as file:
+                self.high_score = file.read()
+        except:
+            print('Missing high score file. Creating one')
+            with open('highscore.txt', 'w') as file:
+                file.write('0')
         self.update_scoreboard()
 
     def update_scoreboard(self):
         self.write(f"Score: {self.score}, high score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def reset(self) -> None:
-        if self.high_score < self.score:
+        if int(self.high_score) < self.score:
             self.high_score = self.score
+            with open('highscore.txt', 'w') as file:
+                file.write(str(self.high_score))
         self.score = 0
         self.clear()
         self.update_scoreboard()
