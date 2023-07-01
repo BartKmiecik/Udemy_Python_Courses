367e69270696a0cc658c5a96db6991c5c83bc737
Bart <kmiecikjob@gmail.com>
2023-07-01 10:53:32 +0200

    Day25
    Counting squirels by colors

--- Day25/main.py
+++ Day25/main.py
@@ -1,6 +1,11 @@
+import pandas
 import pandas as pd
+#
+# weather = pd.read_csv('weather_data.csv')
+# print(weather)
+# temp = weather['temp'].to_list()
+# print(temp)
 
-weather = pd.read_csv('weather_data.csv')
-print(weather)
-temp = weather['temp'].to_list()
-print(temp)
+data = pandas.read_csv('Squirrel_Data.csv')
+color_counts = data['Primary Fur Color'].value_counts()
+print(color_counts)
