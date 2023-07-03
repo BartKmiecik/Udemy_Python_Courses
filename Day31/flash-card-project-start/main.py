08855bbfdd6afae119d4a5df8dd76691fc2026b7
Bart <kmiecikjob@gmail.com>
2023-07-03 19:37:17 +0200

    Day31Adding logic to flip cards by time, adding logic to buttons to remove words from list, reset timer and move to next word

--- Day31/flash-card-project-start/main.py
+++ Day31/flash-card-project-start/main.py
@@ -1,27 +1,41 @@
 from tkinter import *
 import pandas as pd
 import random
+import time
 BACKGROUND_COLOR = "#B1DDC6"
 #---------------------Logic----------------------------
 title_text = 'Language'
 word_text = 'word'
-
+counter = 0
 data = pd.read_csv('data/french_words.csv')
-
+to_learn = data.to_dict(orient='records')
 def right_answer():
-    i = random.randint(0, len(data)-1)
-    title_text = 'French'
-    word_text = f'{data.iloc[i]["French"]}'
-    canvas.itemconfig(lang_canvas, text=title_text)
-    canvas.itemconfig(word_canvas, text=word_text)
+    to_learn.remove(to_learn[counter])
+    next_word()
 
 
 def wrong_answer():
-    i = random.randint(0, len(data) - 1)
-    title_text = 'French'
-    word_text = f'{data.iloc[i]["French"]}'
+    next_word()
+
+def show_answer():
+    canvas.itemconfig(flash_card_img, image=back_img)
+    title_text = 'English'
+    word_text = f'{to_learn[counter]["English"]}'
     canvas.itemconfig(lang_canvas, text=title_text)
     canvas.itemconfig(word_canvas, text=word_text)
+
+def next_word():
+    global counter, flip_timer
+    window.after_cancel(flip_timer)
+    canvas.itemconfig(flash_card_img, image=front_img)
+    counter = random.randint(0, len(to_learn) - 1)
+    title_text = 'French'
+    word_text = f'{to_learn[counter]["French"]}'
+    canvas.itemconfig(lang_canvas, text=title_text)
+    canvas.itemconfig(word_canvas, text=word_text)
+    flip_timer= window.after(3000, show_answer)
+
+
 
 
 #---------------------UI-------------------------------
@@ -30,9 +44,12 @@ window.config(padx=200, pady=200, bg= BACKGROUND_COLOR, borderwidth=0)
 window.minsize(width=800, height=550)
 window.title('Flashcards')
 
+front_img = PhotoImage(file='images/card_front.png')
+back_img = PhotoImage(file='images/card_back.png')
+
 canvas = Canvas(width=800, height=550)
-image = PhotoImage(file='images/card_front.png')
-canvas.create_image(400, 270, image=image)
+card_front_img = PhotoImage(file='images/card_front.png')
+flash_card_img = canvas.create_image(400, 270, image=card_front_img)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0, border=0)
 canvas.grid(column=1, row=1)
 lang_canvas = canvas.create_text(400,200, text= title_text, fill='black', font=('Arial', 24, 'bold'))
@@ -46,5 +63,9 @@ wrong_button.grid(column=0, row=2)
 right_img = PhotoImage(file='images/right.png')
 right_button = Button(image=right_img, highlightthickness=0, borderwidth=0, command=right_answer)
 right_button.grid(column=2, row=2)
+
+flip_timer = window.after(3000, show_answer)
+next_word()
+
 
 window.mainloop()
