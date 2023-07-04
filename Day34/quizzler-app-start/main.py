c599e8de1a70f19e5bb6aa6a99841cd6413e7282
Bart <kmiecikjob@gmail.com>
2023-07-04 16:59:49 +0200

    Day34First step

--- Day34/quizzler-app-start/main.py
+++ Day34/quizzler-app-start/main.py
@@ -1,8 +1,20 @@
 from question_model import Question
-from data import question_data
+#from data import question_data
+from data2 import question_data
 from quiz_brain import QuizBrain
+import requests
 
 question_bank = []
+
+def get_new_questions():
+    request = requests.get('https://opentdb.com/api.php?amount=10&type=boolean')
+    question_data = request.json()['results']
+    with open('data2.py', 'w') as file:
+        file.write('question_data = ' +  str(question_data))
+
+
+get_new_questions()
+
 for question in question_data:
     question_text = question["question"]
     question_answer = question["correct_answer"]
