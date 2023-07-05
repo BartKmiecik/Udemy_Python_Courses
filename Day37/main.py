98bc59706575c7f9aaf03d183db4e9bf66a2b189
Bart <kmiecikjob@gmail.com>
2023-07-05 20:18:18 +0200

    Day37
    Connecting to external server, post some date, create graph

--- Day37/main.py
+++ Day37/main.py
@@ -1,13 +1,31 @@
 import requests
 
 url = 'https://pixe.la/v1/users'
+username = 'bart32123'
+token = 'asdfxcvertdfsg'
+headers = {
+    'X-USER-TOKEN': token
+}
 
 params = {
-    'token': 'asdfxcvertdfsg',
-    'username': 'bart32123',
+    'token': token,
+    'username': username,
     'agreeTermsOfService': 'yes',
     'notMinor': 'yes',
 }
 #
 # request = requests.post(url=url, json=params)
-print(request.text)
+# print(request.text)
+
+creating_graph = f'{url}/{username}/graphs'
+
+graph_params = {
+    'id': 'graph1',
+    'name': 'myGraph1',
+    'unit': 'fucks',
+    'type': 'float',
+    'color': 'momiji'
+}
+
+request = requests.post(url=creating_graph, json=graph_params, headers=headers)
+print(request)
