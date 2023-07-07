653c66c02db471a1a916399d51b6e0ce263e25cc
Bart <kmiecikjob@gmail.com>
2023-07-07 20:58:28 +0200

    Day39Got some flight from France to Poland, but lazy to integrate it :p

--- Day39/flight-deals-start/flight-deals-start/flight_search.py
+++ Day39/flight-deals-start/flight-deals-start/flight_search.py
@@ -3,7 +3,14 @@ import os
 
 os.environ['tequile_api_key'] = 'YgQv2gWX8BR-IpYtW_KHXwHgp7WI6btz'
 headers = {
-    'apikey': os.environ['tequile_api_key']
+    'apikey': os.environ['tequile_api_key'],
+}
+
+pramas = {
+    'fly_from': 'FRA',
+    'date_from': '09/09/2023',
+    'date_to': '10/10/2023',
+    'fly_to': 'PL'
 }
 
 class FlightSearch:
@@ -12,8 +19,8 @@ class FlightSearch:
         key = os.environ['tequile_api_key']
         url = 'https://api.tequila.kiwi.com'
         location_query = '/locations/query'
-        request = requests.get(url=f'{url}{location_query}', headers=headers)
-        print(request)
+        request = requests.get(url=f'{url}/v2/search', params= pramas,headers=headers)
+        print(request.json())
 
 
 
