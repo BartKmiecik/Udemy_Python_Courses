5c058d2b4d4870c992c11e56c3f0ae210f9df79f
Bart <kmiecikjob@gmail.com>
2023-07-07 21:21:00 +0200

    Day39Main class import importing everything (to lazy and to drunk to finishi it today, sorry tommorrow Bart :( )

--- Day39/flight-deals-start/flight-deals-start/main.py
+++ Day39/flight-deals-start/flight-deals-start/main.py
@@ -1,1 +1,5 @@
+from flight_search import FlightSearch
+from data_manager import DataManager
+from flight_data import FlightData
+from notification_manager import  NotificationManager
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
