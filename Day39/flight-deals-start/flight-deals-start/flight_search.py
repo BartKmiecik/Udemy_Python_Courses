6f1812bc7cc54f95c7babd4ed3bdd7c112a02a44
Bart <kmiecikjob@gmail.com>
2023-07-07 20:19:28 +0200

    Day39
    Working on this

--- Day39/flight-deals-start/flight-deals-start/flight_search.py
+++ Day39/flight-deals-start/flight-deals-start/flight_search.py
@@ -1,3 +1,21 @@
+import requests
+import os
+
+os.environ['tequile_api_key'] = 'YgQv2gWX8BR-IpYtW_KHXwHgp7WI6btz'
+headers = {
+    'apikey': os.environ['tequile_api_key']
+}
+
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
-    pass
+    def __init__(self):
+        key = os.environ['tequile_api_key']
+        url = 'https://api.tequila.kiwi.com'
+        location_query = '/locations/query'
+        request = requests.get(url=f'{url}{location_query}', headers=headers)
+        print(request)
+
+
+
+
+search = FlightSearch()
