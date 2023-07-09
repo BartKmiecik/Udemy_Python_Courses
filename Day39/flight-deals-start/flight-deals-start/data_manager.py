f554b19c0483e63c2945faf3c5c40bf095f1d9f4
Bart <kmiecikjob@gmail.com>
2023-07-09 12:32:42 +0200

    Day40
    Modify flight searcher to add feature for adding new users to db

--- Day39/flight-deals-start/flight-deals-start/data_manager.py
+++ Day39/flight-deals-start/flight-deals-start/data_manager.py
@@ -1,8 +1,14 @@
 import requests
 import os
 
+
 os.environ['sheety_url'] = 'https://api.sheety.co/e623837db3ef07fc2dd590b5fd4f0822/flightDeals/sheet1'
 os.environ['sheety_author'] = 'Bearer secret'
+
+header = {
+    'Authorization': 'Bearer secret'
+}
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
     def __init__(self):
@@ -20,3 +26,29 @@ class DataManager:
         # print(self.request.json()['sheet1'])
         return city_price
 
+    def add_user(self, user):
+        id = self.check_row_id()
+        request = requests.put(url=f'https://api.sheety.co/e623837db3ef07fc2dd590b5fd4f0822/flightDeals/users/{id}',json=user, headers=header)
+        print(request.text)
+
+    def check_row_id(self):
+        request = requests.get(url='https://api.sheety.co/e623837db3ef07fc2dd590b5fd4f0822/flightDeals/users', headers=header)
+        users_base = request.json()['users']
+        ids = []
+        for id in users_base:
+            ids.append(int(id['id']))
+        return ids
+
+#
+# date = {
+#     'user':{
+#         'firstName': 'Bart2',
+#         'lastName': 'Kmie2',
+#         'email': 'tttt2'
+#     }
+# }
+#
+# manager = DataManager()
+# #
+# last_id = max(manager.check_row_id())+1
+# manager.update_date(date, last_id)
--- Day39/flight-deals-start/flight-deals-start/main.py
+++ Day39/flight-deals-start/flight-deals-start/main.py
@@ -10,11 +10,12 @@ notificator = NotificationManager()
 city_price = data_manager.get_date()
 print(city_price)
 notificator.send_notification('test')
-for city in city_price:
-    flight_price = flight_search.check_flight_price(city[1])
-    try:
-        if flight_price < city[2]:
-            msg = f'Price for flight to {city[0]} is now {flight_price}'
-            notificator.send_notification(msg)
-    except TypeError:
-        print('Something doesn\'t import correctly from google sheet, just ignore. ')
+# for city in city_price:
+#     flight_price = flight_search.check_flight_price(city[1])
+#     try:
+#         if flight_price < city[2]:
+#             msg = f'Price for flight to {city[0]} is now {flight_price}'
+#             notificator.send_notification(msg)
+#     except TypeError:
+#         print('Something doesn\'t import correctly from google sheet, just ignore. ')
+
