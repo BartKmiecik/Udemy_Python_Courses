e700336a97267d075e4494d5ed0fe67e67dbfcf4
Bart <kmiecikjob@gmail.com>
2023-07-09 16:11:28 +0200

    Day 40
    Upgrading notification manager to send emails as well

--- Day40/flight-deals-end/flight-deals-end/notification_manager.py
+++ Day40/flight-deals-end/flight-deals-end/notification_manager.py
@@ -1,10 +1,13 @@
 from twilio.rest import Client
+import smtplib
 
 TWILIO_SID = 'AC05f0ea84be4dd923f1a3cdc81d27a531'
 TWILIO_AUTH_TOKEN = 'e7483b05f217dfed2fd8255f64030b16'
 TWILIO_VIRTUAL_NUMBER = '+18145262067'
 TWILIO_VERIFIED_NUMBER = '+48737365338'
 
+email = 'mlodybartus@gmail.com'
+password = 'tkelpunbstudhbgo'
 
 class NotificationManager:
 
@@ -19,3 +22,14 @@ class NotificationManager:
         )
         # Prints if successfully sent.
         print(message.sid)
+
+    def send_email(self, message):
+        with smtplib.SMTP('smtp.gmail.com') as connection:
+            connection.starttls()
+            connection.login(email, password)
+            connection.sendmail(email, email, message)
+            connection.quit()
+
+
+
+
